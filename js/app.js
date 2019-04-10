e693b2d55b7af6ca976469e80cf0ae18b257cd7b
jasoniyi <mac@Baruchhabas-mac.local>
2019-04-10 12:22:24 +0100

    added css,js and img folders

---
+++ js/app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+// login modal  popup
+const loginBox = document.getElementById('login');
+const banner_Text = document.getElementById('banner-text');
+const loginCloseicon = document.querySelector('.close');
+const loginModal = document.querySelector('.bg-modal');
+
+// register modal popup
+const registerBox = document.getElementById('register');
+const registerModal = document.querySelector('.bg-reg-modal');
+const registerCloseicon = document.querySelector('.closeReg');
+
+
+// Opens up login Modal popup
+loginBox.addEventListener('click', function () {
+    banner_Text.style.display = 'none';
+	loginModal.style.display = 'flex';
+});
+// Closes the login Modal popup with the closeicon
+loginCloseicon.addEventListener('click', function (){
+	banner_Text.style.display = 'block';
+	loginModal.style.display = 'none';
+});
+
+// Opens up Register Modal popup
+registerBox.addEventListener('click', function () {
+    banner_Text.style.display = 'none';
+	registerModal.style.display = 'block';
+});
+// Closes the Register Modal popup with the Register close icon
+registerCloseicon.addEventListener('click', function (){
+	banner_Text.style.display = 'block';
+	registerModal.style.display = 'none';
+});
+
